120a694cd0d58c6d6cee9c637d412389f0d522aa
Natanael Lima <taelima1997@gmail.com>
2021-12-10 23:37:16 -0300

    add initial Tarot

---
+++ src/components/Tarot/index.tsx
@@ -0,0 +1,49 @@
+import { View, StyleSheet } from "react-native";
+
+import { Card } from "../Card";
+
+const cards = [
+  {
+    source: require("./assets/death.png"),
+  },
+  {
+    source: require("./assets/chariot.png"),
+  },
+  {
+    source: require("./assets/high-priestess.png"),
+  },
+  {
+    source: require("./assets/justice.png"),
+  },
+  {
+    source: require("./assets/lover.png"),
+  },
+  {
+    source: require("./assets/pendu.png"),
+  },
+  {
+    source: require("./assets/tower.png"),
+  },
+  {
+    source: require("./assets/strength.png"),
+  },
+];
+
+export const assets = cards.map((card) => card.source);
+
+export const Tarot = () => {
+  return (
+    <View style={styles.container}>
+      {cards.map((card, index) => (
+        <Card card={card} key={index} index={index} />
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "lightblue",
+  },
+});
