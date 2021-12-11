eb4547ecf159cdd4d9db6daecd7aac017f4bcb29
Natanael Lima <taelima1997@gmail.com>
2021-12-10 23:41:25 -0300

    update imports

--- src/components/Tarot/index.tsx
+++ src/components/Tarot/index.tsx
@@ -1,31 +1,32 @@
+import React from 'react';
 import { View, StyleSheet } from "react-native";
 
 import { Card } from "../Card";
 
 const cards = [
   {
-    source: require("./assets/death.png"),
+    source: require("../../assets/death.png"),
   },
   {
-    source: require("./assets/chariot.png"),
+    source: require("../../assets/chariot.png"),
   },
   {
-    source: require("./assets/high-priestess.png"),
+    source: require("../../assets/high-priestess.png"),
   },
   {
-    source: require("./assets/justice.png"),
+    source: require("../../assets/justice.png"),
   },
   {
-    source: require("./assets/lover.png"),
+    source: require("../../assets/lover.png"),
   },
   {
-    source: require("./assets/pendu.png"),
+    source: require("../../assets/pendu.png"),
   },
   {
-    source: require("./assets/tower.png"),
+    source: require("../../assets/tower.png"),
   },
   {
-    source: require("./assets/strength.png"),
+    source: require("../../assets/strength.png"),
   },
 ];
 
