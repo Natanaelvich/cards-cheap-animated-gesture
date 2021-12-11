1e5e29101e51d32bd918c666d66f95f6724192f8
Natanael Lima <taelima1997@gmail.com>
2021-12-10 23:59:59 -0300

    add initial cadr gesture animation

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,17 +1,22 @@
 import React, { useEffect } from "react";
 import { View, StyleSheet, Dimensions, Image } from "react-native";
-import type { PanGestureHandlerGestureEvent } from "react-native-gesture-handler";
+import { PanGestureHandler, PanGestureHandlerGestureEvent } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
 import { snapPoint } from "react-native-redash";
 
-const { width: wWidth, height } = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
 
-const SNAP_POINTS = [-wWidth, 0, wWidth];
 const aspectRatio = 722 / 368;
-const CARD_WIDTH = wWidth - 128;
+const CARD_WIDTH = width - 128;
 const CARD_HEIGHT = CARD_WIDTH * aspectRatio;
 const IMAGE_WIDTH = CARD_WIDTH * 0.9;
 const DURATION = 250;
-
+const side = (width - CARD_WIDTH) /2
+const SNAP_POINTS = [-side, 0, side];
 interface CardProps {
   card: {
     source: ReturnType<typeof require>;
@@ -20,18 +25,41 @@ interface CardProps {
 }
 
 export const Card = ({ card: { source } }: CardProps) => {
+  const x = useSharedValue(0);
+  const y = useSharedValue(0);
+
+  const onGestureEvent = useAnimatedGestureHandler<PanGestureHandlerGestureEvent, {x : number, y : number}>({
+    onStart: (_, ctx) => {
+      ctx.x = x.value;
+      ctx.y = y.value;
+    },
+    onActive: ({ translationX, translationY }, ctx) => {
+      x.value = ctx.x +  translationX;
+      y.value = ctx.y +  translationY;
+    },
+    onEnd : ({velocityX,velocityY}) => {
+        const dest = snapPoint(x.value, velocityX, SNAP_POINTS)
+    }
+  });
+
+  const style = useAnimatedStyle(() => ({
+    transform: [{ translateX: x.value }, { translateY: y.value }],
+  }));
+
   return (
     <View style={styles.container} pointerEvents="box-none">
-      <View style={[styles.card]}>
-        <Image
-          source={source}
-          style={{
-            width: IMAGE_WIDTH,
-            height: IMAGE_WIDTH * aspectRatio,
-          }}
-          resizeMode="contain"
-        />
-      </View>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View style={[styles.card, style]}>
+          <Image
+            source={source}
+            style={{
+              width: IMAGE_WIDTH,
+              height: IMAGE_WIDTH * aspectRatio,
+            }}
+            resizeMode="contain"
+          />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
   );
 };
