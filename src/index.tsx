4a4a406dc7b5158a6349ec0132f31083a19f2baf
Natanael Lima <taelima1997@gmail.com>
2021-12-10 23:37:37 -0300

    update main component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Tarot } from './components/Tarot';
 
 
 const Main : React.FC = () => {
     return (
-        <View />
+        <Tarot />
     );
 };
 
